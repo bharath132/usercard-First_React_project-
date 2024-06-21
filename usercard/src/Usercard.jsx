4540dd4c135aab6ca6ef4d631583303d0d05756a
bharath132 <chidambaramb2@gmail.com>
2024-06-22 01:18:35 +0530

    completed the project (react )

--- usercard/src/Usercard.jsx
+++ usercard/src/Usercard.jsx
@@ -1,25 +1,82 @@
 // import App from './App.jsx'
-function User(){
-return (<div className="car-contaainer">
-        <p className="pro">online</p>
-        <img className="img" src="https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG-Image.png" alt="" />
-        <h3>full name</h3>
-        <h4>salem</h4>
-        <p>web developer</p>
-        <div className="skills">
-            <ul>
-                <li>skill</li>
-                <li>skill</li>
-                <li>skill</li>
-                <li>skill</li>
-                
-            </ul>
-        </div>
-    </div>)
+const users = [
+  {
+    name:"Vijay Krishna",
+    place:"Chennai",
+    decs:"web developer",
+    skills:[
+      "javascript",
+      "CSS",
+      "HTML",
+      "WEB DESINGING",
+      "UI/UX",
+      "React",
+      "developer"],
+    online:true
+  },
+  {
+    name:"Vijay kumar",
+    place:"salem",
+    decs:"web designer",
+    skills:[
+      "flutter",
+      "figma",
+      "WEB DESINGING",
+      "UI/UX",
+      "React native",
+      "developer"],
+      online:false
+  },
+  {
+    name:"Vijay kumar",
+    place:"salem",
+    decs:"web designer",
+    skills:[
+      "flutter",
+      "figma",
+      "WEB DESINGING",
+      "UI/UX",
+      "React native",
+      "developer"],
+      online:false
+  },
+];
+function User(props) {
+  return (
+    <div className="car-contaainer">
+      <p className= {props.online ? 'pro online' : 'pro offline'} >{props.online ? 'online' : 'offline'}</p>
+      <img
+        className="img"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3flIHsvZtK3eU7tEnp-LSEjNznTZCn0dkcA&s"
+        alt=""
+      />
+      <h3>{props.name}</h3>
+      <h4>{props.place}</h4>
+      <p>{props.decs}</p>
+      <div className="skills">
+        <h2>Skills :</h2>
+        <ul>
+          {props.skills.map((skill, index) => (
+            <li key={index}> {skill} </li>
+          ))
+          }
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export const Usercard = () => {
   return (
-    <User />
-  )
-}
+  <>
+  {users.map((user)=>(
+    <User   name={user.name}
+    place={user.place}
+    decs={user.decs}
+    skills={user.skills}
+    online={user.online}
+    /> 
+  ))}
+  </>
+  );
+};
